40364fe95d4ae4270be22082a68e97ff6a6c330e
Grace Lee <eunheh@gmail.com>
2016-10-03 19:45:13 -0400

    to add Readme.med and hardish

---
+++ Readme.md
@@ -0,0 +1,5 @@
+#Link to personal blog.
+
+Clink on the link below to read about my current journey in to a sea of bits, an ocean of them.
+
+[Medium/@eunheh](https://medium.com/@eunheh)
--- hardish.js
+++ hardish.js
@@ -1,21 +1,16 @@
-var x;
-var y;
-var z;
+
 
 var hardish = function(x,y,z) {
-
   var highest = Math.max(x,y,z);
-  var second = Math.median(x,y,z);
-
-  console.log(Math.sqrt(highest) + Math.sqrt(second));
+  var secondHighest = Math.max(x,y,z) && !highest;
+  return Math.sqrt(highest + secondHighest);
 }
 
 function(2,3,4);
 
 
 
-var n;
-n <= 0;
-function {
-  console.log(n -= 1);
+var zero = function(countdown) {
+for (var x; x >= 0 ; x -= 1);
+  console.log(x -=1);
 }
